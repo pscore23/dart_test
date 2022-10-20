93f4cd36e69f53c28c23439aea1f1bc7fd56e43b
pscore23 <rubik2357x@gmail.com>
2022-10-20 20:26:34 +0900

    [update] テスト

--- lib/test.dart
+++ lib/test.dart
@@ -1,3 +1,18 @@
 void main() {
-  print("Hello, World!");
+  int i = 0;
+  double j = 1.00;
+  String text = "World";
+  bool flag = false;
+
+  dynamic any = "!";
+
+  final nc = "Hello";
+  const cp = ", ";
+
+  print("${nc}${cp}${text}${any}"); // こういうコメント書いてみたかった
 }
+
+/*
+Dart すごい
+よって Flutter もすごい
+*/
